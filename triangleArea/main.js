ca70b8a410d1d82d91102ae35ef497407551e15d
lucasmbaca <lucasmbaca96@gmail.com>
2023-07-18 23:49:09 -0400

    (feat): triangle area calculate js file

---
+++ triangleArea/main.js
@@ -0,0 +1,27 @@
+
+const calculate = (e) => {
+    e.preventDefault();
+
+    const num1 = parseFloat(document.getElementById('lado1').value);
+    const num2 = parseFloat(document.getElementById('lado2').value);
+    const num3 = parseFloat(document.getElementById('lado3').value);
+
+    let suma = (num1 + num2 + num3)/2
+    let area =  Math.sqrt(suma*((suma-num1)*(suma-num2)*(suma-num3)));
+    
+    const li = document.createElement('li');
+    li.innerHTML = area;
+
+    document.querySelector('#resultado').append(li);
+
+    document.getElementById('lado1').value = '';
+    document.getElementById('lado2').value = '';
+    document.getElementById('lado3').value = '';
+
+    return false
+    
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('form').onsubmit = calculate;
+});
